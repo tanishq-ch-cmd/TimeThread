8c4bd53df936bc4733648972ca17f0323ece7789
Tanishq Chauhan <tanishqchauhan9393@gmail.com>
2026-08-26 11:22:35 +0530

    Initial Spring Boot setup with hello endpoint

---
+++ src/main/java/com/tanishqchcmd/timethread/HelloController.java
@@ -0,0 +1,18 @@
+package com.tanishqchcmd.timethread;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+        @RestController
+public class HelloController
+{
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello, TimeThread!";}
+    @GetMapping("/hello")
+    String sayMeow(){
+
+        {
+            return"Meow meow";
+        }
+    }
+}
