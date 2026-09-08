b1292cfad61c8b27e59c3381a8cb82cbbdddb5d7
Tanishq Chauhan <tanishqchauhan9393@gmail.com>
2026-09-08 20:06:54 +0530

    Error fixing for final Railway app run

--- src/main/java/com/tanishqchcmd/timethread/SecurityConfig.java
+++ src/main/java/com/tanishqchcmd/timethread/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/register", "/students/register", "/css/**", "/js/**", "/h2-console/**").permitAll()
+                        .requestMatchers("/", "/login", "/register", "/css/**", "/images/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
